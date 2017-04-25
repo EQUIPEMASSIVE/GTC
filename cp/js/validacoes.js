88af1edc9303959c36735376dda2498417dd84e8
SteveJobsApple <williamdreuxx@gmail.com>
2017-04-24 23:58:32 -0300

    dELTuDO

--- cp/js/validacoes.js
+++
@@ -1,9 +0,0 @@
-function validarFormCat(){
-	var campo_cat = document.getElementById("categorias-nomes").value;
-
-	if(campo_cat == ""){
-
-		alert("Informe uma categoria para cadastrar");
-		return false;
-	}
-}
--- cp/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['usuario']);
-unset($_SESSION['senha']);
-
-header("Location: index.php");
-
-?>
