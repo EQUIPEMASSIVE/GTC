7d0126cd03d0c5b357f4c1cd33dc398439b8602a
victorwilliames <williamesdj@gmail.com>
2017-04-25 14:19:25 -0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -114,8 +114,10 @@
     <nav class="navbar navbar-toggleable-md navbar-dark fixed-top scrolling-navbar">
                <div class="container">
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav1" aria-expanded="false" aria-label="Toggle navigation">
-                <a class="navbar-brand" href="index.php"><img src="../img/logoIndex.png" />AFE</a>
+               <span class="navbar-toggler-icon"></span> 
             </button>
+                   
+                   <a class="navbar-brand" href="index.php"><img src="../img/logoIndex.png" /></a>
 
 
             <div class="collapse navbar-collapse" id="navbarNav1">
