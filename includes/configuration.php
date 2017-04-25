6fa0f6068e4eb7b6ba9b870962ef68fd3df05fbd
victorwilliames <williamesdj@gmail.com>
2017-04-25 02:48:15 -0300

    Update configuration.php

--- includes/configuration.php
+++ includes/configuration.php
@@ -2,9 +2,9 @@
 //PROPRIEDADES DB MYSQL
 
 define("HOST", "mysql796.umbler.com");
-//define("USER",  "u263627493_root");
-//define("PASS", "root123456"); 
-//define("DBNAME", "u263627493_gtc");
+define("USER",  "u263627493_root");
+define("PASS", "root123456"); 
+define("DBNAME", "u263627493_gtc");
 
 // As informações abaixo é para usar em offline/local, quando for subir esse arquivo para nuvem,
 // mudar para  essas definições acima: 
