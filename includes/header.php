747fd35b91f6d7864cfe706a885468eef6bb89a0
HesronZT4 <hesroncarvalho99@gmail.com>
2017-04-30 22:59:15 -0300

    up

--- includes/header.php
+++ includes/header.php
@@ -126,10 +126,10 @@
 
 
 <body>
-
+    <div class="container">
     <!--Navbar-->
     <nav class="navbar navbar-toggleable-md navbar-dark fixed-top scrolling-navbar">
-               <div class="container">
+               
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav1" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span> 
             </button>
