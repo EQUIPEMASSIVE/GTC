b4d29afeabee941db15501dd50119fb6eadc13a2
Beron99 <hesroncarvalho99@gmail.com>
2017-05-08 14:36:30 -0300

    up

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 
         
 <br><hr>
-<h3><span class="label label-default">Útimas Notícias</span></h3>
+
 
 <main>
     <br>
@@ -58,7 +58,7 @@
 
 
             <!--First row-->
-           
+           <h3><span class="label label-default">Útimas Notícias</span></h3>
                 <div class="col-md-12">
                 <!--Card-->
                 <div class="card wow fadeIn"  data-wow-delay="0.3s">
