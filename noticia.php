231967d579dfea4c0830344eb18bd3e9d31d52e1
HesronZT4 <hesroncarvalho99@gmail.com>
2017-05-14 11:14:56 -0300

    bugfixing
    Sistema de comentario usando o plugin do fecebook está no ar! 100%

--- noticia.php
+++ noticia.php
@@ -109,15 +109,22 @@
 
 
 
-<div class="fb-comments" data-href="http://www.guiatecnologico.tk/noticia.php" data-width="100%" data-numposts="30"></div>
+<?php  
+
+$url="http://www.guiatecnologico.16mb.com/noticia.php?id=".$_GET['id']."";
+echo '<div class="fb-comments" data-href="'.$url.'" data-num-posts="2" data-width="470"></div>';
+
+
+?>
+
 
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.9";
-  fjs.parentNode.insertBefore(js, fjs);
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) return;
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+ fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
 
