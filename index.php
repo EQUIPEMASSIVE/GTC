c94845d93a8ff47d09462e1322ce85287b8d8de4
Beron99 <hesroncarvalho99@gmail.com>
2017-05-17 15:09:35 -0300

    up

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
                 <!--First row-->
            
-                <div class="col-md-12">
+                <div class="col-md-6">
                 <!--Card-->
                 <div class="card wow fadeIn"  data-wow-delay="0.3s">
 
