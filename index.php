9d7203c0acd4426290c5a6b7eb5aa1dc0ff1a62a
HesronZT4 <hesroncarvalho99@gmail.com>
2017-05-21 10:59:18 -0300

    fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             <div>
                 <a href="noticia.php?id=<?php echo $bn['id_noticia']; ?>" ><img src="cp/imagens/imgnoticia/<?php echo $bn['imagem']; ?>" alt="Postagem 1"/>
 
-                <div  style="position: absolute; top: 350px; left: 40px; white-space: normal;"><!--nome do titulo da noticia por id-->
+                <div  style="position: absolute; top: 320px; left: 40px; white-space: normal;"><!--nome do titulo da noticia por id-->
                   <h1 style="font-size: 50px; text-align: center; text-shadow: 1px 1px 2px rgba(0,0,0,.7); "> 
                     <div href="noticia.php?id=<?php echo $bn['id_noticia']; ?>" style="color: white;"><?php echo $bn['titulo'];?></div>
                     <div>
@@ -38,7 +38,7 @@
                 </div>
 
 
-                <div  style="position: absolute; top: 300px; left: 40px;"><!--texto slider categoria da noticia por id-->
+                <div  style="position: absolute; top: 280px; left: 40px;"><!--texto slider categoria da noticia por id-->
                   <h1 style="font-size: 35px; text-align: center; text-shadow: 1px 1px 2px rgba(0,0,0,.7);"> 
                     <b><div href="noticia.php?id=<?php echo $bn['id_noticia']; ?>" style="color: #cd2323; text-shadow: 1px 1px 2px rgba(0,0,0,.7);"><?php echo $bn['nome_categoria'];?></div></b>  
                     <div>
