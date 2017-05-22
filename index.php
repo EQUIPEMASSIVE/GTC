50f7679c3257e383774c7007c71b9920cabdda50
victorwilliames <williamesdj@gmail.com>
2017-05-22 16:22:28 -0300

    tst2

--- index.php
+++ index.php
@@ -282,7 +282,7 @@
 
     <nav id="navegacao" style="text-align: center;">
         <?php
-        echo '<a href="?pagina=1" class="page-link" aria-label="Previous">
+        echo '<a href="?pagina=1" class="btn btn-danger" class="page-link" aria-label="Previous">
                 <span aria-hidden="true">Primeira</span>
                 <span class="sr-only">Primeira</span>
               </a> ';
