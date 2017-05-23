b707f3dfa963f2692ba946e84f3b227a1b75e69e
Beron99 <hesroncarvalho99@gmail.com>
2017-05-23 18:11:22 -0300

    up

--- index.php
+++ index.php
@@ -212,7 +212,7 @@
 
                         <h1 class="h1-responsive"> <a href="noticia.php?id=<?php echo $pusha['id_noticia']; ?>" style="color: #000" class="card-title"><?php echo $pusha['titulo'];?></a></h1> 
                         
-
+                        <br>      
                         <!--Text-->
                        
                         <!--<a href="noticia.php?id=<?php echo $pusha['id_noticia']; ?>" class="black-text d-flex flex-row-reverse"><h7 class="waves-effect p-2">Leia mais... <i class="fa fa-chevron-right"></i></h7></a>-->
