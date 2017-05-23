1887b2bff661128dbe0d3a2883aa42323c9f8c98
HesronZT4 <hesroncarvalho99@gmail.com>
2017-05-23 19:24:47 -0300

    fixxx

--- index.php
+++ index.php
@@ -208,7 +208,7 @@
                     <!--Card content-->
                     <div class="card-block" style="text-align:center">
                         <!--Title-->
-                        <h5 class="price"><a href="categoria.php?id=<?php echo $pusha['id_categoria']; ?>"> <span class="badge btn-elegant"> <?php echo $pusha['nome_categoria']; ?></span></a></h5>  
+                        <!--h5 class="price"><a href="categoria.php?id=<?php //echo $pusha['id_categoria']; ?>"> <span class="badge btn-elegant"> <?php //echo $pusha['nome_categoria']; ?></span></a></h5>--> 
 
                         <h1 class="h1-responsive"> <a href="noticia.php?id=<?php echo $pusha['id_noticia']; ?>" style="color: #000" class="card-title"><?php echo $pusha['titulo'];?></a></h1> 
                         
