a904f4df307f54480be3319f9da1c21c1f3c9d83
Beron99 <hesroncarvalho99@gmail.com>
2017-05-24 11:49:28 -0300

    tes

--- index.php
+++ index.php
@@ -218,9 +218,9 @@
                     </div>
                     <!--/.Card content-->
                      
-
+                <span style="color: #C0C0C0" ><i class="fa fa-clock-o" ></i> Publicado dia: <?php echo $pusha['datapub'];?></span>
                 </div>
-                 <span style="color: #C0C0C0" ><i class="fa fa-clock-o" ></i> Publicado dia: <?php echo $pusha['datapub'];?></span>
+                 
                 <!--/.Card--> <br> <br> 
             </div>
             <!--First columnn-->
