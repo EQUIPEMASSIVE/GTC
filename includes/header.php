4f1866b3f8391944aa717493c71f16dfd5727e22
Victor Williames <williamesdj@gmail.com>
2017-05-26 10:06:10 -0300

    DONT PANIC!  Deploy automatizado ON!

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>ttsz NÃO ENTRE EM PANICO! GZUZ XAMA REGUERO (92% Concluido) v0.2-beta </title>
+    <title>FONT PANIC! (92% Concluido) v0.2-beta </title>
      
      
  
