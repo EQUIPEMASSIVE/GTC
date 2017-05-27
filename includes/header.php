b921eb7465972e31c273b7e9f39f596b46525553
alvaroreis <alvaroluiz.rb@gmail.com>
2017-05-27 14:07:28 -0300

    update header

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>DDONT PANIC!! (92% Concluido) v0.2-beta </title>
+    <title>DDONT PANIC!! (94% Concluido) v0.2-beta </title>
      
      
  
