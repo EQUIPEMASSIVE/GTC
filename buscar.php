4eb0d19dd9a1a8ce570487cce80643eb14c1bf73
HesronZT4 <hesroncarvalho99@gmail.com>
2017-05-27 19:10:58 -0300

    fff

--- buscar.php
+++ buscar.php
@@ -25,7 +25,7 @@
             
             <hr>
         <div class="row">
-                <div class="col-lg-6">
+                <div class="col-md-6">
 
 
                     <!--Card Content-->
