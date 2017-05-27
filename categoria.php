985651df8e48519bb756f17aa6937d0d26c7acb6
HesronZT4 <hesroncarvalho99@gmail.com>
2017-05-27 18:56:06 -0300

    fff

--- categoria.php
+++ categoria.php
@@ -23,15 +23,6 @@
 
 
 
-       
-      
-
-
-
-
-
-
-
         $id_cat = $_GET["id"];
                                                                                       //Exibe a noticia em ordem descrecente por id da noticia
         $SQL_NC = mysql_query("SELECT * FROM noticias WHERE categoria=$id_cat AND status='1' ORDER BY id_noticia DESC");
@@ -73,45 +64,39 @@
                 <div class="card-block" style="min-height: 400px; max-width: auto; background-color: white; padding-bottom: 10px;">
 
 
-                    <!--Card image-->
-                    <div class="card-block view overlay hm-zoom">  
+                    <!--Card Content-->
+                   
+                        <!--Title-->
+                        <h1><a href="noticia.php?id=<?php echo $src['id_noticia'];  ?>"><?php echo $src['titulo']; ?></a></h1>
+                        <p>Por: <?php echo $src['autorPub']; ?></p>
+
+                                    <!--Card Image-->
+                     <div class="card-block view overlay hm-zoom">  
+                        <img src="cp/imagens/imgnoticia/<?php echo $src['imagem']; ?>" alt="<?php echo $src['titulo']; ?>">
+                        
+                       </div> 
                     
-                        <a href="noticia.php?id=<?php echo $id_news;?>"><img src="cp/imagens/imgnoticia/<?php echo $imagem_news; ?>"  class="img-responsive img-fluid " alt="Titulo na Notícia" /></a>                          
-
-                            <div class="mask waves-effect waves-light"></div>
-                        </a>
-                    </div>
-                    <!--/.Card image-->
-
-                
-              <!--Card content-->
-                    <div class="card-block" style="text-align:center; background-color: white;">
-                     <span style="color: #C0C0C0" ><i class="fa fa-clock-o" ></i> Publicado dia: <?php echo $dataEX;?></span>
-
-                        <!--Title ARRUMA O BUG DE PUXAR CATEGORIA AQUI NESSA LINHA ABAIXO -->
-                        <h5 class="price"><a href="categoria.php?id=<?php echo $id_cat; ?>"> <!--<span class="badge btn-elegant"> <?php echo $rn['nome_categoria']; ?></span></h5>   </a>-->
-
-                        <h1 class="h1-responsive"> <a href="noticia.php?id=<?php echo $id_news;?>" style="color: #000" class="card-title"><?php echo $titulo_news;?></h1> </a>
-                                     
-
-
-                        <!--Text-->
-                       <!-- <p style="color: #696969" class="card-text"><?php echo  substr($conteudo_news, 0, 900); ?>...</p>-->
-                        <br>
-                        
                       
+
+                                    <!--TEXTO-->
+                  
+                        <p><?php echo substr($src['conteudo'], 0, 0); ?></p>
+                        
+                        
                         
 
-                       
+                        
+                    
+
+                    
+                   <a type="button" class="btn btn-primary btn-lg btn-block"  href="noticia.php?id=<?php echo $src['id_noticia']; ?>">...Leia Mais</a>
+                   
+                   
+            <!--/Post data-->
 
 
-
-                            
-
-                
-            </div>
-
-</div>
+                </div>
+          </div>
 
           
 
